257a258819b6f6063fd4bac5373ec81f4a1e9cb2
lukasschirren <l.schirren@web.de>
2024-11-27 10:43:15 +0000

    Better readibility

--- hydropower_prep_EU.py
+++ hydropower_prep_EU.py
@@ -16,7 +16,6 @@ import pandas as pd
 import geopandas as gpd
 from pyproj import Transformer
 
-# Function to convert each coordinate
 def convert_coordinates(easting, northing):
     longitude, latitude = transformer.transform(easting, northing)
     return latitude, longitude
@@ -29,19 +28,15 @@ if __name__ == "__main__":
     os.makedirs(output_dir, exist_ok=True) 
     output_path = os.path.join(output_dir, "hydropower_dams_EU.gpkg")
 
-    # Read data from CSV
     data = pd.read_csv(input_path)
 
-    # Select relevant columns
     data = data[['id', 'lat', 'lon', 'name', 'type', 
                  'installed_capacity_MW', 'avg_annual_generation_GWh', 
                  'head', 'country_code']]
 
-    # Ensure numeric conversion for relevant columns
     data['lon'] = pd.to_numeric(data['lon'], errors='coerce')
     data['lat'] = pd.to_numeric(data['lat'], errors='coerce')
 
-    # Rename columns to match conventions
     data = data.rename(columns={
         "installed_capacity_MW": "capacity",
         "lat": "Latitude",
@@ -49,18 +44,14 @@ if __name__ == "__main__":
         "type": "plant_type"
     })
 
-    # Drop rows with missing coordinates
     data = data.dropna(subset=['Longitude', 'Latitude'])
 
-    ######## Data Preparation ########
     data = data[data['plant_type'].isin(['HDAM', 'HPHS'])]
     data['capacity'] = pd.to_numeric(data['capacity'], errors='raise')
 
-    # Filter for existing plants
     data_existing = data.dropna(subset=['head'])
     print(f"Number of missing 'head' values: {data_existing['head'].isna().sum()}")
 
-    ######## Export GeoPackage ########
     gdf = gpd.GeoDataFrame(
         data_existing,
         geometry=gpd.points_from_xy(data_existing.Longitude, data_existing.Latitude)
@@ -69,6 +60,5 @@ if __name__ == "__main__":
     gdf.set_crs(epsg=4326, inplace=True)
     gdf.to_file(output_path, layer='dams', driver="GPKG")
 
-    # Display where the file is stored (relative path)
     relative_output_path = os.path.relpath(output_path, script_dir)
     print(f"GeoPackage file successfully created at: {relative_output_path}")
